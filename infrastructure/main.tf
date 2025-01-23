0322dc27effbcf692c269627115321928ecb230c
Tom Potter <tom.potter@bbc.co.uk>
2025-01-23 13:58:48 +0000

    added pr update and reopen events to trigger build/test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -152,6 +152,16 @@ resource "aws_codebuild_webhook" "example" {
     }
 
     filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_UPDATED"
+    }
+
+    filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_REOPENED"
+    }
+
+    filter {
       type    = "BASE_REF"
       pattern = "main"
     }
