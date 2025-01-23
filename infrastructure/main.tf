c9ee3e6e1ab5160340198e64a41afce32b476f7a
Tom Potter <tom.potter@bbc.co.uk>
2025-01-23 16:01:58 +0000

    testing if duplicate auth is needed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -141,19 +141,19 @@ resource "aws_codebuild_project" "project" {
 }
 
 # Authenticating connection to GitHub
-resource "aws_codestarconnections_connection" "example" {
-  name          = "example-connection"
-  provider_type = "GitHub"
-}
+# resource "aws_codestarconnections_connection" "example" {
+#   name          = "example-connection"
+#   provider_type = "GitHub"
+# }
 
-import {
-  to = aws_codestarconnections_connection.example
-  id = "arn:aws:codeconnections:eu-west-2:093380438279:connection/811fdea6-493f-421e-904c-16b3b156ccee"
-}
+# import {
+#   to = aws_codestarconnections_connection.example
+#   id = "arn:aws:codeconnections:eu-west-2:093380438279:connection/811fdea6-493f-421e-904c-16b3b156ccee"
+# }
 
 # Activating CodeBuild on GitHub push events
 resource "aws_codebuild_webhook" "example" {
-  depends_on = [aws_codestarconnections_connection.example]
+  # depends_on = [aws_codestarconnections_connection.example]
   project_name = var.project_name
   build_type   = "BUILD"
 
